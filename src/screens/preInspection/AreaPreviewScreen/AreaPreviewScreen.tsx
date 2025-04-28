4ba16b31a188ba9c06e43e7c19cf17eae46d41f0
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-04-28 15:23:30 -0300

    feat: base area preview

--- src/screens/preInspection/AreaPreviewScreen/AreaPreviewScreen.tsx
+++ src/screens/preInspection/AreaPreviewScreen/AreaPreviewScreen.tsx
@@ -1,14 +1,40 @@
-import React from "react";
-import { Text } from "react-native";
-import { Screen } from "../../../components/Screen/Screen";
-import { useTranslation } from "react-i18next";
+import React from 'react';
+import {ActivityIndicator, Text, TouchableOpacity, View} from 'react-native';
+import {Screen} from '../../../components/Screen/Screen';
+import {useTranslation} from 'react-i18next';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {PreInspectionStackParamsList} from '../../../routes/PreInspectionRoutes';
 
-export function AreaPreviewScreen() {
-  const {t} = useTranslation()
+type ScreenProps = NativeStackScreenProps<
+  PreInspectionStackParamsList,
+  'AreaPreviewScreen'
+>;
+export function AreaPreviewScreen({route}: ScreenProps) {
+  const {coords} = route.params;
+  const {t} = useTranslation();
 
   return (
     <Screen screenTitle={t('areaPreview')} showBackButton>
-      <Text>Area Preview</Text>
+      <View className="w-full h-[250px] bg-red-500" />
+
+      <View className="p-3 rounded-2xl border mt-5">
+        <Text>{t('coordinates')}</Text>
+        {coords.map((item, index) => (
+          <Text key={index} className="mb-1">
+            Lat: {item.latitude}, Lng: {item.longitude}
+          </Text>
+        ))}
+      </View>
+
+      <TouchableOpacity
+        className="w-full h-[48] bg-[#229B13] flex items-center justify-center rounded-2xl mt-10"
+      >
+        {false ? (
+          <ActivityIndicator color="white" size={30} />
+        ) : (
+          <Text className="font-semibold text-white">{t('startInspection')}</Text>
+        )}
+      </TouchableOpacity>
     </Screen>
-  )
+  );
 }
