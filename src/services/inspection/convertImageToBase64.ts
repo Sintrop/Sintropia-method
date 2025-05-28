6e9568e310921369b38805a3f6924e7afacaa274
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-05-28 14:15:46 -0300

    feat: add react native image resizer

--- src/services/inspection/convertImageToBase64.ts
+++ src/services/inspection/convertImageToBase64.ts
@@ -1,14 +1,11 @@
 import RNFS from 'react-native-fs';
-import { Image } from 'react-native-compressor';
+import ImageResizer from 'react-native-image-resizer';
 
 export async function convertImageToBase64(uri: string): Promise<string> {
   if ( !uri.includes('file://') ) return ''
 
-  const imageCompressed = await Image.compress(uri, {
-    compressionMethod: 'auto',
-    quality: 0.1,
-  })
+  const resized = await ImageResizer.createResizedImage(uri, 100, 130, 'JPEG', 60);
 
-  const base64 = await RNFS.readFile(imageCompressed, 'base64');
+  const base64 = await RNFS.readFile(resized.uri, 'base64');
   return 'data:image/jpeg;base64,' + base64;
 }
